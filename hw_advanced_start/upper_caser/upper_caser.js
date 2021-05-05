9bfa82a73ebd77d76493107c2c1656e44f53f749
rock-007 <umair.ashraf2019@gmail.com>
2021-05-05 18:34:44 +0100

    Uppercases test passed

---
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -0,0 +1,9 @@
+const UpperCaser = function (words) {
+    this.words = words
+}
+
+UpperCaser.prototype.toUpperCase = function () {
+    return this.words.map(eachItem=>eachItem.toUpperCase())
+}
+
+module.exports = UpperCaser;
