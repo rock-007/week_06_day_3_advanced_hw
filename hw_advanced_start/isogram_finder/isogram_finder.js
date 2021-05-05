6687bc6a187a55d0c4e5af0aade3cbce94cda9ba
rock-007 <umair.ashraf2019@gmail.com>
2021-05-05 20:45:51 +0100

    all isogram test passes

--- hw_advanced_start/isogram_finder/isogram_finder.js
+++ hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,8 +1,22 @@
 const IsogramFinder = function (word) {
-
+    this.pangram = 'qwertyuiopasdfghjklzxcvbnm'.split('')
+    this.alphabet = word.toLowerCase().split('').filter(eachAlphabet=>{return eachAlphabet != " " || this.pangram.includes(eachAlphabet) })
+    
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+    let tempPangram = this.pangram
+    return this.alphabet.every((eachAlphabet)=>{
+        result =false
+
+        if (tempPangram.includes(eachAlphabet)){
+            result =true;
+            let alphabetIndex = tempPangram.indexOf(eachAlphabet)
+            tempPangram.splice(alphabetIndex,1)
+        }
+        return result
+
+    })
 
 }
 
